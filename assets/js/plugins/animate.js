2af11cb9d6439f4daa6a50630cf65ebe6bb6e21f
Luis_Julio <luislmorningstrar@gmail.com>
2022-03-03 05:12:23 -0300

    correção de imagens

--- assets/js/plugins/animate.js
+++ assets/js/plugins/animate.js
@@ -1,5 +1,8 @@
 
 let animationArea = document.querySelector('.anime');
+let aAc = document.querySelector('.contact');
+let aAw = document.querySelector('.work');
+
 
 
     let animationObject = bodymovin.loadAnimation({
@@ -10,3 +13,31 @@ let animationArea = document.querySelector('.anime');
             autoloadSegments: false,
             path:"https://assets7.lottiefiles.com/packages/lf20_isbiybfh.json"
     });
+
+
+
+let aObject = bodymovin.loadAnimation({
+        container: aAc,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        autoloadSegments: false,
+        path:"https://assets4.lottiefiles.com/packages/lf20_9wjm14ni.json"
+});
+
+
+let aObjectw = bodymovin.loadAnimation({
+        container: aAw,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        autoloadSegments: false,
+        path:"https://assets10.lottiefiles.com/packages/lf20_7smeegra.json"
+});
+
+
+
+
+
+
+
